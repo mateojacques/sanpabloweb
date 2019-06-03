236e190bd91137f3b5dfd5f9e1cfbad0f81f6f7a
Mateo Jacques <teojacques182@gmail.com>
2019-06-03 12:34:18 -0300

    v26

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,8 +12,13 @@ const desktop = window.matchMedia("(min-width: 1900px)");
 
 window.onload = function () {
 
-    document.getElementById("payment__button").href = '';
-    document.getElementById("payment__button").style.backgroundColor = 'grey';
+    try {
+        document.getElementById("payment__button").href = '';
+        document.getElementById("payment__button").style.backgroundColor = 'grey';
+    } catch {
+
+        
+    }
 
     if (mobile.matches) {
         headerImg[0].src = "https://www.officemax.co.nz/Images/ProductImages/2182149.jpg";
@@ -46,12 +51,12 @@ window.onresize = function () {
 function openNav() {
     document.getElementById("nav--side").style.width = "250px";
     document.getElementById("nav--side").style.borderLeft = "5px solid #132a5c";
-  }
+}
 
 function closeNav() {
     document.getElementById("nav--side").style.width = "0px";
     document.getElementById("nav--side").style.borderLeft = "0px";
-  }
+}
 
 function headerIn() {
 
@@ -95,7 +100,7 @@ function headerOut() {
     headerImg[0].style.opacity = '1';
 }
 
-function unlockBtn(){
+function unlockBtn() {
     document.getElementById("payment__button").href = "https://www.mercadopago.com/mla/checkout/start?pref_id=146908791-e6619a38-10f7-473c-a1a7-221d0cc6e146";
     document.getElementById("payment__button").style.backgroundColor = '#132a5c';
 }
