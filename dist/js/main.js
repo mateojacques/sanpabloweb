4232cd387f21925dabb2421c1a6d11fc633ae359
Mateo Jacques <teojacques182@gmail.com>
2019-06-08 17:15:15 -0300

    v63

--- dist/js/main.js
+++ dist/js/main.js
@@ -653,5 +653,22 @@ function drawSelect() {
 function submitData() {
     const price = document.getElementById('form__price');
     localStorage.setItem('price', price.value);
-    console.log(localStorage);
+}
+
+function help(){
+    const popup = document.querySelector('.form__help__popup');
+    const cross = document.querySelector('.form__help__exit');
+
+    popup.style.display = 'flex';
+    popup.style.opacity = 1;
+    cross.style.opacity = 1;
+}
+
+function helpOut(){
+    const popup = document.querySelector('.form__help__popup');
+    const cross = document.querySelector('.form__help__exit');
+
+    popup.style.display = 'none';
+    popup.style.opacity = 0;
+    cross.style.opacity = 0;
 }
