22a0827ae618c6b8f2e14c0d73f3e3137d4b5ab9
Mateo Jacques <teojacques182@gmail.com>
2019-06-20 11:35:04 -0300

    v89

---
+++ dist/res/Documentos/Code/Python/Twitter API/auth.py
@@ -0,0 +1,12 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> consumer_key = 'gj3evQNZKKmjjhtH44HnB6ifm';
+consumer_secret = 'oG4AUYN3kVaDtbS4pGCf3W3x9ypBtn50oYvnT6t3LUicDnSYwx';
+access_token = '1096270563918692353-Sk3YuwCxlya3AB8HWRT1QZw5YHW4Iy';
+access_token_secret = 'iLgZaIBlmKV7pC2VEYOfUJ11CNguosu4NIkmy0bhjnbjO';
+SyntaxError: multiple statements found while compiling a single statement
+>>> consumer_key = 'gj3evQNZKKmjjhtH44HnB6ifm';
+>>> consumer_secret = 'oG4AUYN3kVaDtbS4pGCf3W3x9ypBtn50oYvnT6t3LUicDnSYwx';
+>>> access_token = '1096270563918692353-Sk3YuwCxlya3AB8HWRT1QZw5YHW4Iy';
+>>> access_token_secret = 'iLgZaIBlmKV7pC2VEYOfUJ11CNguosu4NIkmy0bhjnbjO';
+>>> 
